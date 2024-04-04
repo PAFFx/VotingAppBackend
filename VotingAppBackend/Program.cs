d430dad22afa68bd2674bbe33b4197e4db8e02c9
Jittabun Banrerngsri <jj2jom@gmail.com>
2024-04-04 15:18:15 +0700

    Establish Mongo connection

--- VotingAppBackend/Program.cs
+++ VotingAppBackend/Program.cs
@@ -1,3 +1,16 @@
+using MongoDB.Driver;
+
+// establish mongo connection
+var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+if (connectionString == null)
+{
+    Console.WriteLine("MONGODB_URI env var not found");
+    Environment.Exit(0);
+}
+var client = new MongoClient(connectionString);
+
+
+// builder
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
